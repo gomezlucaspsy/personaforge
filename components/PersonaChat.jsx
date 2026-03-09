2f48fbe30708dcb087b18bf840ccf2270de0829c
Lucas <lucas.gomez@mi.unc.edu.ar>
2026-03-09 03:25:28 -0300

    updates

--- components/PersonaChat.jsx
+++ components/PersonaChat.jsx
@@ -548,9 +548,6 @@ export default function PersonaChat() {
         <div className="p3wm">ORBITAL FREEBASE // SOCIAL LINK OS</div>
         <div className="p3sys">
           <span className="p3time">{clock.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" })}</span>
-          <span className="p3chip active">Home</span>
-          <span className="p3chip">Explore</span>
-          <span className="p3chip">Library</span>
         </div>
         <div className="p3theme">
           <span className="p3theme-label">Theme</span>
